5994ed56cc2cc20150c863903471ce2f839be03a
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-15 18:16:06 +0100

    windowManager

---
+++ src/main/java/me/frogdog/core/EngineManager.java
@@ -0,0 +1,4 @@
+package me.frogdog.core;
+
+public class EngineManager {
+}
--- src/main/java/me/frogdog/test/Launcher.java
+++ src/main/java/me/frogdog/test/Launcher.java
@@ -1,10 +1,18 @@
 package me.frogdog.test;
 
+import me.frogdog.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
 
     public static void main(String[] args) {
-        System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("Frog Engine", 0, 0, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
